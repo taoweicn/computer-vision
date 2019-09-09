8c6e153c28a531e5122cb929b73d16e73196160f
taoweicn <twchn@live.com>
2019-09-09 18:16:05 +0800

    ✨ feat: add perceptron

---
+++ perceptron/solution.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(12)
+num_observations = 500
+
+x1 = np.random.multivariate_normal([0, 0], [[1, 0.75], [0.75, 1]], num_observations)
+x2 = np.random.multivariate_normal([1, 4], [[1, 0.75], [0.75, 1]], num_observations)
+X = np.vstack((x1, x2)).astype(np.float32)
+y = np.hstack((-np.ones(num_observations), np.ones(num_observations)))
+
+
+# 感知机函数
+def perceptron_sgd_plot(X, Y):
+    w = np.zeros(len(X[0]))
+    eta = 0.8
+    epochs = 30
+    errors = []
+    for t in range(epochs):
+        total_error = 0
+        for i, x in enumerate(X):
+            if (np.dot(X[i], w) * Y[i]) <= 0:
+                total_error += (np.dot(X[i], w) * Y[i])
+                w = w + eta * X[i] * Y[i]
+        errors.append(total_error * -1)
+    plt.plot(errors)
+    plt.xlabel('Epoch')
+    plt.ylabel('Total Loss')
+    plt.show()
+    return w
+
+
+w = perceptron_sgd_plot(X, y)
+
+for d, sample in enumerate(X):
+    if d < num_observations:
+        plt.scatter(sample[0], sample[1], c='r', s=120, marker='_')
+    else:
+        plt.scatter(sample[0], sample[1], c='b', s=120, marker='+')
+
+x2 = [w[0], w[1], -w[1], w[0]]
+x3 = [w[0], w[1], w[1], -w[0]]
+
+x2x3 = np.array([x2, x3])
+X, Y, U, V = zip(*x2x3)
+ax = plt.gca()
+ax.quiver(X, Y, U, V, scale=1, color='blue')
+plt.show()
